e07ab5b608c563670f0bd30b5c939f1b5a1c0f36
matthias-prevost <matthias.prevost@gmail.com>
2023-10-18 16:08:43 +0200

    fix Image.network test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,26 +1,19 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter_test/flutter_test.dart';
+import 'package:network_image_mock/network_image_mock.dart';
 
 import 'package:project0/main.dart';
 
 void main() {
-  testWidgets('My widget is a list of image and titles',
+  testWidgets('My widget displays a title, image and instructions',
       (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const CocktailDetail(
-      name: "Margarita",
-      instructions: "Lorem ipsum...",
-      imageURL:
-          "https://www.thecocktaildb.com/images/media/drink/5noda61589575158.jpg",
-    ));
+    await mockNetworkImagesFor(
+        () async => await tester.pumpWidget(const CocktailDetail(
+              name: "Margarita",
+              instructions: "Lorem ipsum...",
+              imageURL:
+                  "https://www.thecocktaildb.com/images/media/drink/5noda61589575158.jpg",
+            )));
 
-    // Verify that our counter starts at 0.
     expect(find.text('Margarita'), findsOneWidget);
     expect(find.text('Lorem ipsum...'), findsOneWidget);
   });
