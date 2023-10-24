e6b805454a2ad246c03e709c3e3b8751f6c49627
matthias-prevost <matthias.prevost@gmail.com>
2023-10-24 17:01:53 +0200

    add firebase core plugin

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,15 @@ import 'package:flutter/material.dart';
 import 'package:project0/widgets/FavCounter.widget.dart';
 import 'package:project0/widgets/Favorites.widget.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(ProviderScope(child: const NavigationBarApp()));
 }
 
