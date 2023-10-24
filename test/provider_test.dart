532a6c473970fed871fad7ba37e2b5c4ebc20894
matthias-prevost <matthias.prevost@gmail.com>
2023-10-24 11:40:57 +0200

    test providers

---
+++ test/provider_test.dart
@@ -0,0 +1,47 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:project0/models/Favorites.dart';
+
+import 'utility.dart';
+
+void main() {
+  test('favorites provider has 0 items on init', () {
+    final container = createContainer();
+
+    expect(
+      container.read(favoriteNotifier).numberOfFavorites,
+      equals(0),
+    );
+  });
+
+  test('can add and remove favorites to provider', () {
+    final container = createContainer();
+    final subscription =
+        container.listen(favoriteNotifier, (previous, next) {});
+
+    expect(
+      container.read(favoriteNotifier).numberOfFavorites,
+      equals(0),
+    );
+
+    subscription.read().add("1");
+    subscription.read().add("1");
+    subscription.read().add("2");
+    expect(
+      container.read(favoriteNotifier).numberOfFavorites,
+      equals(2),
+    );
+
+    subscription.read().remove("2");
+    subscription.read().remove("3");
+    expect(
+      container.read(favoriteNotifier).numberOfFavorites,
+      equals(1),
+    );
+
+    subscription.read().remove("1");
+    expect(
+      container.read(favoriteNotifier).numberOfFavorites,
+      equals(0),
+    );
+  });
+}
