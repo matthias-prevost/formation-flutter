ec1d1d65b28172cd1ef14d3b2689354f54c87f72
matthias-prevost <matthias.prevost@gmail.com>
2023-10-26 16:45:38 +0200

    fix ios bug

--- lib/main.dart
+++ lib/main.dart
@@ -15,9 +15,7 @@ import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await Firebase.initializeApp();
 
   runApp(ProviderScope(child: const App()));
 }
